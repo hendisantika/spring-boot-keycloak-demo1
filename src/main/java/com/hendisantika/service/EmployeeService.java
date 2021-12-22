f7815b217fe42db2b1154c1fc8d2ad4af4e1def3
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-22 10:29:52 +0700

    #7 - Add Employee Service

---
+++ src/main/java/com/hendisantika/service/EmployeeService.java
@@ -0,0 +1,56 @@
+package com.hendisantika.service;
+
+import com.hendisantika.entity.Employee;
+import com.hendisantika.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/10/21
+ * Time: 05.50
+ */
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostConstruct
+    public void initializeEmployeeTable() {
+        employeeRepository.saveAll(
+                Stream.of(
+                        new Employee("naruto", 20000),
+                        new Employee("sasuke", 55000),
+                        new Employee("sakura", 120000),
+                        new Employee("kakashi", 120000),
+                        new Employee("hinata", 120000),
+                        new Employee("kiba", 120000),
+                        new Employee("shino", 120000),
+                        new Employee("asuma", 120000),
+                        new Employee("shikamaru", 120000),
+                        new Employee("choji", 120000),
+                        new Employee("ino", 120000)
+                ).collect(Collectors.toList()));
+    }
+
+    public Employee getEmployee(int employeeId) {
+        return employeeRepository
+                .findById(employeeId)
+                .orElse(null);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository
+                .findAll();
+    }
+}
