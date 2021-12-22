27f0e1a3ce435ffbf8370fe8868b5c9edfeceb27
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-22 10:29:14 +0700

    #4 - Add UserDTO

---
+++ src/main/java/com/hendisantika/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-demo1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/12/21
+ * Time: 13.11
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class UserDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
