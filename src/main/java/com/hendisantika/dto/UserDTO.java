eee9b66c9fc12e1cdc49090b2b0d9181d715a6c7
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 13:13:11 +0700

    #18 - Add phone & dob on UserDTO

--- src/main/java/com/hendisantika/dto/UserDTO.java
+++ src/main/java/com/hendisantika/dto/UserDTO.java
@@ -19,6 +19,8 @@ public class UserDTO {
     private String username;
     private String password;
     private String email;
+    private String phone;
+    private String dob;
     private String firstName;
     private String lastName;
     private String description;
